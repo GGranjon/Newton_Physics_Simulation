7e56ef200d7c7684fe2c7bc3ad0035be1a0534f4
Gabin Granjon <gabin.granjon@telecomnancy.eu>
2024-10-04 05:20:39 +0200

    ajout de collisions, ajout de generation de systemes solaires, ajout deplacement dans la simulation

--- main.cpp
+++ main.cpp
@@ -5,11 +5,14 @@
 #include <ctime>
 #include <math.h>
 #include <fstream>
-#define cst_g 6.6743*10e-7
+#define G 6.6743*10e-7
 #define fps 60
 #define simulation_time 200
-#define pos_min -1000
-#define pos_max 1000
+#define pos_min -1500
+#define pos_max 1500
+#define v_min -200
+#define v_max 200
+#define PI 3.14159265
 #define source "data/data.txt"
 using namespace std;
 
@@ -20,33 +23,88 @@ struct corps
     double vx;
     double vy;
     long long int masse;
-    int rayon;
-    string name;
+    int radius;
+    string type;
 };
 
 struct star : corps{
-    star(double x_, double y_, string name_){
+    star(double x_, double y_, string type_){
         masse = 10e12;
         x = x_;
         y = y_;
         vx = 0.;
         vy = 0.;
-        name = name_;
-        rayon = 800;
+        type = type_;
+        radius = 100;
     }
 };
 
 struct planet : corps{
-    planet(double x_, double y_, double vx_, double vy_, string name_){
+    planet(double x_, double y_, double vx_, double vy_, string type_){
         masse = 2e10;
         x = x_;
         y = y_;
         vx = vx_;
         vy = vy_;
-        name = name_;
-        rayon = 50;
+        type = type_;
+        radius = 10;
     }
 };
+
+double to_radians(double degres){
+    return degres*PI/180;
+}
+
+void rotation(vector<double>& coords, float theta){
+    /*en degrés, sens horaire*/
+    double x = coords[0];
+    double y = coords[1];
+    double rad_theta = to_radians(theta);
+    coords[0] = x*cos(rad_theta)-y*sin(rad_theta);
+    coords[1] = x*sin(rad_theta)+y*cos(rad_theta);
+}
+
+vector<vector<double>> genere_liste_planets(int nb, vector<double>& center, int h, double space, double speed){
+    /*h la hauteur d'une branche (nb de points qu'on y place), b le nombre de branches autour du point, space l'espacement entre chaque points*/
+    vector<vector<double>> objects;
+    objects.resize(nb);
+    vector<double> v_init = {speed, 0};
+    int compteur = nb;
+    int b = (nb%h != 0)? nb/h + 1 : nb/h;
+    double theta = 360./(double)b;
+    vector<double> coords;
+    vector<double> speeds;
+    vector<double> object;
+    double x, y, vx, vy;
+    
+    for (int i = 0; i<b;i++){
+        for (int j = 0; j<h;j++){
+            if (i*h+j+1<=nb){
+                y = center[1] + (j+1)*space;
+                x = center[0];
+                vx = speed;//+(speed/h)*j;
+                vy = 0;
+                speeds = {vx,vy};
+                coords = {x,y};
+                rotation(speeds, i*theta);
+                rotation(coords, i*theta);
+                object = {coords[0], coords[1], speeds[0], speeds[1]};
+                objects[nb - compteur] = object;
+                compteur--;
+            }
+        }
+    }
+    return objects;
+}
+
+void init_galaxy(vector<corps*>& current_objects, int nb_planets, star* star, int h, double space, double speed){
+    vector<double> center = {star->x, star->y};
+    vector<vector<double>> liste_planets = genere_liste_planets(nb_planets, center, h, space, speed);
+    for (vector<double> obj : liste_planets){
+        corps* ptr = new planet({obj[0], obj[1], obj[2], obj[3], "planete"});
+        current_objects.push_back(ptr);
+    }
+}
 
 double distance(const corps& a, const corps& b){
     return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
@@ -56,25 +114,41 @@ void update(vector<corps*>& current_positions){
     double dt = 3.0 / fps;
     double force_x, force_y, dist, accel;
 
-    for (corps* main_corps : current_positions) {
+    for (size_t i = 0; i < current_positions.size(); ++i) {
+
         force_x = 0;
         force_y = 0;
 
-        for (corps* other_corps : current_positions) {
-            if (main_corps != other_corps) {
+        for (size_t j = 0; j < current_positions.size(); ++j) {
 
-                dist = distance(*main_corps, *other_corps);
-                accel = (cst_g * other_corps->masse) / (dist * dist);
+            dist = distance(*current_positions[i], *current_positions[j]);
+            if (dist < 1e-5) {
+                dist = 1e-5;  // Utilise une petite valeur pour éviter la division par zéro
+            }
+            // If a duplicate is found, erase the duplicate element
 
+            if (i!=j && dist <= 2*(current_positions[i]->radius + current_positions[j]->radius)/3) { //collision
+                double total_mass = current_positions[i]->masse + current_positions[j]->masse;
+                current_positions[i]->vx = (current_positions[i]->vx * current_positions[i]->masse + current_positions[j]->vx * current_positions[j]->masse) / total_mass;
+                current_positions[i]->vy = (current_positions[i]->vy * current_positions[i]->masse + current_positions[j]->vy * current_positions[j]->masse) / total_mass;
+                current_positions[i]->type = (current_positions[i]->masse > current_positions[j]->masse) ? current_positions[i]->type : current_positions[j]->type;
+                current_positions[i]->masse = total_mass;
+                current_positions[i]->radius = sqrt(pow(current_positions[i]->radius,2)+pow(current_positions[j]->radius,2));
+                current_positions.erase(current_positions.begin() + j);
+                --j;
+            }
+
+            else{
+                accel = (G * current_positions[j]->masse) / (dist * dist);
                 // Projection sur x et y
-                force_x += accel * ((other_corps->x - main_corps->x) / dist);
-                force_y += accel * ((other_corps->y - main_corps->y) / dist);
+                force_x += accel * ((current_positions[j]->x - current_positions[i]->x) / dist);
+                force_y += accel * ((current_positions[j]->y - current_positions[i]->y) / dist);
             }
         }
-
-        main_corps->vx += force_x * dt;
-        main_corps->vy += force_y * dt;
+        current_positions[i]->vx += force_x * dt;
+        current_positions[i]->vy += force_y * dt;
     }
+
 
     // Mise à jour des positions
     for (corps* main_corps : current_positions) {
@@ -96,10 +170,10 @@ void write_data(string src, vector<vector<corps*>>& all_data){
         file << "[";
         for (corps* obj : temps){
             if (obj != temps[temps.size() -1]){
-                file << "(" + to_string(obj->x) +","+ to_string(obj->y) +","+ to_string(obj->vx) +","+ to_string(obj->vy) +","+ to_string(obj->masse) +","+ to_string(obj->rayon) +","+ obj->name + "),";
+                file << "(" + to_string(obj->x) +","+ to_string(obj->y) +","+ to_string(obj->vx) +","+ to_string(obj->vy) +","+ to_string(obj->masse) +","+ to_string(obj->radius) +","+ obj->type + "),";
             }
             else{
-                file << "(" + to_string(obj->x) +","+ to_string(obj->y) +","+ to_string(obj->vx) +","+ to_string(obj->vy) +","+ to_string(obj->masse) +","+ to_string(obj->rayon) +","+ obj->name + ")";
+                file << "(" + to_string(obj->x) +","+ to_string(obj->y) +","+ to_string(obj->vx) +","+ to_string(obj->vy) +","+ to_string(obj->masse) +","+ to_string(obj->radius) +","+ obj->type + ")";
             }
         }
         file << "]\n";
@@ -118,12 +192,10 @@ void init_randomly(int nb_planets, int nb_stars, vector<corps*>& current_objects
     double x_val, y_val, v_x, v_y;
     for (int i = 0; i< nb_planets; i++){    //initializing each object
         x_val = rdm_value(pos_min, pos_max);
-        cout<<x_val;
         y_val = rdm_value(pos_min, pos_max);
-        v_x = rdm_value(pos_min/10, pos_max/10);
-        v_y = rdm_value(pos_min/10, pos_max/10);
+        v_x = rdm_value(v_min, v_max);
+        v_y = rdm_value(v_min/10, v_max);
         corps* ptr = new planet({x_val,y_val,v_x,v_y,"planete-"+to_string(i)});
-        cout<<ptr->y;
         current_objects.push_back(ptr);
     }
     for (int i = 0; i< nb_stars; i++){    //initializing each object
@@ -141,12 +213,17 @@ int main(int argc, char const *argv[])
     int nb_planets, nb_stars;
     vector<corps*> current_objects;
     vector<vector<corps*>> all_data;
+
     cout << "Combien de planetes?\n";
     cin >> nb_planets;
-    cout << "Combien d'etoiles?\n";
-    cin >> nb_stars;
+    /*cout << "Combien d'etoiles?\n";
+    cin >> nb_stars;*/
+
     srand((time(NULL)));
-    init_randomly(nb_planets, nb_stars, current_objects);
+    star* etoile = new star({0,0,"etoile"});
+    //init_randomly(nb_planets, nb_stars, current_objects);
+    init_galaxy(current_objects, nb_planets, etoile, 5, 200, 350);
+    current_objects.push_back(etoile);
     int nb_frames = fps*simulation_time;
     for (int i = 0; i<nb_frames; i++){
         update(current_objects);
--- pygame_simulation.py
+++ pygame_simulation.py
@@ -33,22 +33,51 @@ def read_data(file_path):
         print(f"Erreur lors de la lecture du fichier: {str(e)}")
         sys.exit(1)
 
-# Fonction pour afficher les planètes dans Pygame
 def visualize_data(frames):
     pygame.init()
 
     # Dimensions de la fenêtre
-    width, height = 1200, 900
-    screen = pygame.display.set_mode((width, height))
+    screen_width = pygame.display.Info().current_w
+    screen_height = pygame.display.Info().current_h
+    screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN)
     pygame.display.set_caption('Simulation de planètes')
+    
+    # Position de la caméra (initialement centrée)
+    camera_x, camera_y = 0, 0
+    camera_speed = 10
 
+    # Charger l'image de fond
+    background_image = pygame.image.load("img/background.jpg")  # Remplace par le chemin de ton image
+    #background_image = pygame.transform.scale(background_image, (screen_width, screen_height))  # Redimensionner si nécessaire
+
+    # Obtenir la taille de l'image d'origine
+    image_width, image_height = background_image.get_size()
+
+    # Calculer le rapport d'aspect de l'image et de l'écran
+    image_aspect = image_width / image_height
+    screen_aspect = screen_width / screen_height
+
+    # Redimensionner l'image tout en gardant son rapport d'aspect
+    if image_aspect > screen_aspect:
+        new_width = screen_width
+        new_height = int(screen_width / image_aspect)
+    else:
+        new_height = screen_height
+        new_width = int(screen_height * image_aspect)
+
+    # Redimensionner l'image
+    background_image = pygame.transform.scale(background_image, (new_width, new_height))
+
+    # Calculer la position pour centrer l'image
+    image_x = (screen_width - new_width) // 2
+    image_y = (screen_height - new_height) // 2
     # Paramètres de visualisation
     bg_color = (0, 0, 0)  # Couleur de fond noire
 
     # Échelle pour adapter les coordonnées des planètes à l'écran
-    scale_factor = 0.05
+    scale_factor = 0.2
     x_shift = 0
-    y_shift = 3000 #pixels
+    y_shift = 0 #pixels
     clock = pygame.time.Clock()
     frame_index = 0
 
@@ -59,7 +88,24 @@ def visualize_data(frames):
             if event.type == pygame.QUIT:
                 running = False
 
-        screen.fill(bg_color)  # Remplir le fond
+        # Gestion des touches pour le déplacement (ZQSD)
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_z]:  # Z pour aller en haut
+            camera_y += camera_speed
+        if keys[pygame.K_s]:  # S pour aller en bas
+            camera_y -= camera_speed
+        if keys[pygame.K_q]:  # Q pour aller à gauche
+            camera_x += camera_speed
+        if keys[pygame.K_d]:  # D pour aller à droite
+            camera_x -= camera_speed
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False  # Arrêter la simulation
+
+        #screen.fill(bg_color)  # Remplir le fond
+        screen.fill((0,0,0))
+        screen.blit(background_image, (0, 0))
 
         # Récupérer les positions de la frame actuelle
         if frame_index < len(frames):
@@ -69,21 +115,29 @@ def visualize_data(frames):
                 planet_color = (12+i, 253-i, 27+i)
 
                 # Convertir les coordonnées à l'échelle de la fenêtre
-                screen_x = int(width / 2 + (x+x_shift) * scale_factor)
-                screen_y = int(height / 2 - (y+y_shift) * scale_factor)  # inverser y pour correspondre à Pygame
-                pygame.draw.circle(screen, planet_color, (screen_x, screen_y), radius/50)
+                screen_x = int(screen_width / 2 + (x+x_shift) * scale_factor)
+                screen_y = int(screen_height / 2 - (y+y_shift) * scale_factor)  # inverser y pour correspondre à Pygame
+
+                # Position de la planète relative à la caméra
+                planet_x = screen_x + camera_x
+                planet_y = screen_y + camera_y
+                if "planete" in name:
+                    pygame.draw.circle(screen, "red", (planet_x, planet_y), radius/5)
+                else:
+                    pygame.draw.circle(screen, "white", (planet_x, planet_y), radius/5)
+
             
             frame_index += 1
         else:
             frame_index = 0  # Réinitialiser à la première frame après la dernière
 
         pygame.display.flip()  # Mettre à jour l'affichage
-        clock.tick(350)  # Limiter à 30 FPS
+        clock.tick(30)  # Limiter à 30 FPS
 
     pygame.quit()
 
 # Charger les données depuis le fichier et démarrer la visualisation
 
-file_path = "data/2corps1astre.txt"
+file_path = "data/data.txt"
 frames = read_data(file_path)
 visualize_data(frames)
